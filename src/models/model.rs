bf9ff2981c2081c031860b93aad87426933cd82e
LimpidCrypto <97235361+LimpidCrypto@users.noreply.github.com>
2023-03-07 22:00:25 +0100

    initial

--- src/models/model.rs
+++ src/models/model.rs
@@ -1,16 +1,16 @@
 //! Base model
 
-use super::exceptions::XRPLModelException;
+use anyhow::Result;
 
 /// A trait that implements basic functions to every model.
 pub trait Model {
-    /// Extended in structures to define custom validation logic.
-    fn get_errors(&self) -> Result<(), XRPLModelException> {
+    /// Collects a models errors and returns the first error that occurs.
+    fn get_errors(&self) -> Result<()> {
         Ok(())
     }
 
-    /// Panics if a object is invalid.
-    fn validate(&self) -> Result<(), XRPLModelException> {
+    /// Simply forwards the error from `get_errors` if there was one.
+    fn validate(&self) -> Result<()> {
         match self.get_errors() {
             Ok(_no_error) => Ok(()),
             Err(error) => Err(error),
--- src/models/transactions/mod.rs
+++ src/models/transactions/mod.rs
@@ -23,6 +23,7 @@ pub mod set_regular_key;
 pub mod signer_list_set;
 pub mod ticket_create;
 pub mod trust_set;
+pub mod exceptions;
 
 pub use account_delete::*;
 pub use account_set::*;
@@ -33,6 +34,7 @@ pub use deposit_preauth::*;
 pub use escrow_cancel::*;
 pub use escrow_create::*;
 pub use escrow_finish::*;
+pub use exceptions::*;
 pub use nftoken_accept_offer::*;
 pub use nftoken_burn::*;
 pub use nftoken_cancel_offer::*;
