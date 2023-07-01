316a12222a46b187e84c9ca0185a47c18f1c194e
LimpidCrypto <info@limpidcrypto.de>
2023-07-01 15:51:28 +0200

    fix cargo clippy

--- src/asynch/clients/websocket_base.rs
+++ src/asynch/clients/websocket_base.rs
@@ -26,12 +26,12 @@ where
 
 pub trait WebsocketClose {
     /// Closes the websocket stream and disconnects from the host.
-    async fn close(&self) -> Result<()>;
+    async fn close(&mut self) -> Result<()>;
 }
 
 pub trait WebsocketIo {
     /// Writes the request to the stream.
-    async fn write<R: Model + Serialize>(&self, request: &R) -> Result<()>;
+    async fn write<R: Model + Serialize>(&mut self, request: &R) -> Result<()>;
 
     /// Read messages from the stream.
     async fn read(&mut self) -> Result<Option<ReadResult<'_>>>;
