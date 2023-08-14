cf1ba5cfa894624247a4769e16325f2d73a5b313
LimpidCrypto <info@limpidcrypto.de>
2023-08-14 22:11:03 +0200

    add AsyncWebsocketClient example

--- examples/std/src/bin/tokio/net/send_request.rs
+++ examples/std/src/bin/tokio/net/send_request.rs
@@ -1,4 +1,32 @@
+use xrpl::asynch::client::{AsyncWebsocketClientTungstenite, TungsteniteMessage};
+use xrpl::models::requests::AccountInfo;
+
 // TODO: add as soon as `AsyncWebsocketClient` is implemented
-fn main() {
-    todo!()
+#[tokio::main]
+async fn main() {
+    let websocket =
+        AsyncWebsocketClientTungstenite::open("wss://xrplcluster.com/".parse().unwrap())
+            .await
+            .unwrap();
+    assert!(websocket.is_open());
+
+    let account_info = AccountInfo::new(
+        "rJumr5e1HwiuV543H7bqixhtFreChWTaHH",
+        None,
+        None,
+        None,
+        None,
+        None,
+        None,
+    );
+
+    websocket.send(&account_info).await.unwrap();
+
+    while let Ok(Some(TungsteniteMessage::Text(response))) = websocket.try_next().await {
+        let account_info_echo: AccountInfo = serde_json::from_str(response.as_str()).unwrap();
+        println!("account_info_echo: {:?}", account_info_echo);
+
+        websocket.close().await.unwrap();
+        break;
+    }
 }
