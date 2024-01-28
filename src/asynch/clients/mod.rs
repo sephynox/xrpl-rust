e272d93d506a6038de78e7a5480fb39f10eec58a
LimpidCrypto <konermann@limpidcrypto.com>
2024-01-28 13:21:30 +0000

    fix ambiguous name error

--- src/asynch/clients/mod.rs
+++ src/asynch/clients/mod.rs
@@ -6,9 +6,9 @@ pub struct WebsocketClosed;
 #[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
 mod embedded_websocket;
 #[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
-mod tokio_tungstenite;
+mod tungstenite;
 
 #[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
 pub use embedded_websocket::*;
 #[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
-pub use tokio_tungstenite::*;
+pub use tungstenite::*;
