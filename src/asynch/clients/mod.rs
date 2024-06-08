6277eb8c2c16441014547fb5c65e5587f9e72c72
LimpidCrypto <konermann@limpidcrypto.com>
2024-06-08 23:16:19 +0000

    adjust clients mod

--- src/asynch/clients/mod.rs
+++ src/asynch/clients/mod.rs
@@ -1,14 +1,5 @@
-pub mod exceptions;
+mod async_client;
+mod client;
+mod websocket;
 
-pub struct WebsocketOpen;
-pub struct WebsocketClosed;
-
-#[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
-mod embedded_websocket;
-#[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
-mod tungstenite;
-
-#[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
-pub use embedded_websocket::*;
-#[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
-pub use tungstenite::*;
+pub use websocket::*;
