08621098b37b0b31701e23926404467ee0ecc087
LimpidCrypto <konermann@limpidcrypto.com>
2024-06-08 23:20:42 +0000

    revise the tests to work with the changes

--- tests/integration/clients/mod.rs
+++ tests/integration/clients/mod.rs
@@ -1,12 +1,9 @@
-use anyhow::anyhow;
 use anyhow::Result;
 
 #[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
 pub async fn test_websocket_tungstenite_echo() -> Result<()> {
     use super::common::connect_to_wss_tungstinite_echo;
-    use futures_util::{SinkExt, TryStreamExt};
-    use xrpl::asynch::clients::TungsteniteMessage;
-    use xrpl::models::requests::AccountInfo;
+    use xrpl::{asynch::clients::XRPLWebsocketIO, models::requests::AccountInfo};
 
     let mut websocket = connect_to_wss_tungstinite_echo().await?;
     let account_info = AccountInfo::new(
@@ -19,37 +16,29 @@ pub async fn test_websocket_tungstenite_echo() -> Result<()> {
         None,
     );
 
-    websocket.send(&account_info).await?;
-    while let Ok(Some(TungsteniteMessage::Text(response))) = websocket.try_next().await {
-        let account_info_echo = serde_json::from_str::<AccountInfo>(response.as_str());
-        match account_info_echo {
-            Ok(account_info_echo) => {
-                assert_eq!(account_info, account_info_echo);
-                return Ok(());
-            }
-            Err(err) => {
-                return Err(anyhow!("Error parsing response: {:?}", err));
-            }
-        };
+    websocket.xrpl_send(account_info).await.unwrap();
+    loop {
+        let t = websocket
+            .xrpl_receive::<AccountInfo<'_>, AccountInfo<'_>>()
+            .await
+            .unwrap();
+        dbg!("{:?}", t);
     }
-
-    Ok(())
 }
 
 #[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
 pub async fn test_embedded_websocket_echo() -> Result<()> {
-    use super::common::{codec::Codec, connect_to_ws_embedded_websocket_tokio_echo};
+    use super::common::connect_to_ws_embedded_websocket_tokio_echo;
     use tokio_util::codec::Framed;
-    use xrpl::asynch::clients::EmbeddedWebsocketReadMessageType;
+    use xrpl::asynch::clients::codec::Codec;
+    use xrpl::asynch::clients::XRPLWebsocketIO;
     use xrpl::models::requests::AccountInfo;
 
     let tcp_stream = tokio::net::TcpStream::connect("ws.vi-server.org:80")
         .await
-        .map_err(|_| anyhow!("Error connecting to websocket"))?;
-    let mut framed = Framed::new(tcp_stream, Codec::new());
-    let mut buffer = [0u8; 4096];
-    let mut websocket =
-        connect_to_ws_embedded_websocket_tokio_echo(&mut framed, &mut buffer).await?;
+        .unwrap();
+    let mut framed = Framed::new(tcp_stream, Codec);
+    let mut websocket = connect_to_ws_embedded_websocket_tokio_echo(framed).await?;
     let account_info = AccountInfo::new(
         None,
         "rJumr5e1HwiuV543H7bqixhtFreChWTaHH".into(),
@@ -59,42 +48,13 @@ pub async fn test_embedded_websocket_echo() -> Result<()> {
         None,
         None,
     );
-    websocket
-        .send(&mut framed, &mut buffer, false, &account_info)
-        .await?;
+    websocket.xrpl_send(account_info).await?;
 
-    let mut ping_counter = 0;
-    loop {
-        match websocket.try_next(&mut framed, &mut buffer).await? {
-            Some(message) => match message {
-                EmbeddedWebsocketReadMessageType::Ping(_) => {
-                    ping_counter += 1;
-                    if ping_counter > 1 {
-                        return Err(anyhow!("Expected only one ping"));
-                    }
-                }
-                EmbeddedWebsocketReadMessageType::Text(text) => {
-                    match serde_json::from_str::<AccountInfo>(text) {
-                        Ok(account_info_echo) => {
-                            assert_eq!(account_info, account_info_echo);
-                            return Ok(());
-                        }
-                        Err(err) => {
-                            return Err(anyhow!("Error parsing response: {:?}", err));
-                        }
-                    }
-                }
-                EmbeddedWebsocketReadMessageType::Binary(_) => {
-                    panic!("Expected text message found binary")
-                }
-                EmbeddedWebsocketReadMessageType::Pong(_) => {
-                    panic!("Expected text message found pong")
-                }
-                EmbeddedWebsocketReadMessageType::Close(_) => {
-                    panic!("Expected text message found close")
-                }
-            },
-            None => return Err(anyhow!("No message received")),
-        }
-    }
+    let t = websocket
+        .xrpl_receive::<AccountInfo<'_>, AccountInfo<'_>>()
+        .await
+        .unwrap();
+    dbg!("{:?}", t);
+    panic!();
+    Ok(())
 }
