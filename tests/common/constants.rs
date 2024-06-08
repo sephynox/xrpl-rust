7c64058415fff470c1e3bcd8416fcd73a3103fd2
LimpidCrypto <konermann@limpidcrypto.com>
2024-06-08 23:20:07 +0000

    add xrpl testnet uri

--- tests/common/constants.rs
+++ tests/common/constants.rs
@@ -1,2 +1,4 @@
-pub const ECHO_WS_SERVER: &'static str = "ws://ws.vi-server.org/mirror/";
-pub const ECHO_WSS_SERVER: &'static str = "wss://ws.vi-server.org/mirror/";
+pub const ECHO_WS_SERVER: &'static str = "ws://ws.vi-server.org/mirror";
+pub const ECHO_WSS_SERVER: &'static str = "wss://ws.vi-server.org/mirror";
+
+pub const XRPL_TEST_NET: &'static str = "wss://testnet.xrpl-labs.com/";
