a38acb964885e737d4561ca8be023d3bf78cff84
LimpidCrypto <konermann@limpidcrypto.com>
2024-06-11 19:25:10 +0000

    fix github workflow build and tests

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -5,18 +5,22 @@ mod integration;
 
 use anyhow::Result;
 
+#[cfg(any(feature = "tungstenite", all(feature = "embedded-ws", feature = "std")))]
 #[tokio::test]
 async fn test_asynch_clients() -> Result<()> {
     #[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
     return integration::clients::test_websocket_tungstenite_test_net().await;
-    #[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
+    #[cfg(all(feature = "embedded-ws", feature = "std", not(feature = "tungstenite")))]
     return integration::clients::test_embedded_websocket_echo().await;
+    Ok(())
 }
 
+#[cfg(any(feature = "tungstenite", feature = "embedded-ws", feature = "std"))]
 #[tokio::test]
 async fn test_asynch_clients_request() -> Result<()> {
-    #[cfg(all(feature = "tungstenite", not(feature = "embedded-ws")))]
+    #[cfg(all(feature = "tungstenite", feature = "std", not(feature = "embedded-ws")))]
     return integration::clients::test_websocket_tungstenite_request().await;
-    #[cfg(all(feature = "embedded-ws", not(feature = "tungstenite")))]
+    #[cfg(all(feature = "embedded-ws", feature = "std", not(feature = "tungstenite")))]
     return integration::clients::test_embedded_websocket_request().await;
+    Ok(())
 }
