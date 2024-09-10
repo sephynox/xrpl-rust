ac839b862b800cf4be4e70ed5a72c6d51642f290
LimpidCrypto <konermann@limpidcrypto.com>
2024-09-10 19:17:00 +0000

    add sign_and_submit example

--- examples/std/src/bin/asynch/transaction/sign_and_submit.rs
+++ examples/std/src/bin/asynch/transaction/sign_and_submit.rs
@@ -1,10 +1,15 @@
-use xrpl::asynch::transaction::sign;
+use xrpl::asynch::clients::AsyncJsonRpcClient;
+use xrpl::asynch::transaction::sign_and_submit;
+use xrpl::asynch::wallet::generate_faucet_wallet;
 use xrpl::models::transactions::account_set::AccountSet;
-use xrpl::wallet::Wallet;
 
-fn main() {
+#[tokio::main]
+async fn main() {
+    let client = AsyncJsonRpcClient::connect("https://testnet.xrpl-labs.com/".parse().unwrap());
     // Create a new wallet we can use to sign the transaction
-    let wallet = Wallet::create(None).unwrap();
+    let wallet = generate_faucet_wallet(&client, None, None, None, None)
+        .await
+        .unwrap();
     // Define the transaction we want to sign
     let mut account_set = AccountSet::new(
         wallet.classic_address.clone().into(),
@@ -27,6 +32,9 @@ fn main() {
         None,
     );
     println!("AccountSet transaction before signing: {:?}", account_set);
-    sign(&mut account_set, &wallet, false).unwrap();
+    // Sign and submit the transaction
+    sign_and_submit(&mut account_set, &client, &wallet, true, true)
+        .await
+        .unwrap();
     println!("AccountSet transaction after signing: {:?}", account_set);
 }
