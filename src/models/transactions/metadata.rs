4d25cd5c6b24eacf14c869aa8f0b9928d7506e89
LimpidCrypto <konermann@limpidcrypto.com>
2024-11-19 13:15:43 +0000

    test get_nftoken_id utility

--- src/models/transactions/metadata.rs
+++ src/models/transactions/metadata.rs
@@ -3,6 +3,8 @@ use alloc::{borrow::Cow, vec::Vec};
 use serde::{Deserialize, Serialize};
 use serde_with::skip_serializing_none;
 
+use crate::models::ledger::objects::LedgerEntryType;
+use crate::models::requests::LedgerIndex;
 use crate::models::Amount;
 
 #[skip_serializing_none]
@@ -26,90 +28,142 @@ pub struct NFTokenMetadataFields<'a> {
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct Fields<'a> {
-    pub flags: i32,
-    pub sequence: i32,
     pub account: Option<Cow<'a, str>>,
-    pub low_limit: Option<BTreeMap<Cow<'a, str>, Cow<'a, str>>>,
-    pub high_limit: Option<BTreeMap<Cow<'a, str>, Cow<'a, str>>>,
     pub balance: Option<Cow<'a, str>>,
-    pub taker_gets: Option<Cow<'a, str>>,
-    pub taker_pays: Option<Cow<'a, str>>,
     pub book_directory: Option<Cow<'a, str>>,
     pub expiration: Option<i32>,
+    pub flags: Option<i32>,
+    pub low_limit: Option<BTreeMap<Cow<'a, str>, Cow<'a, str>>>,
+    pub high_limit: Option<BTreeMap<Cow<'a, str>, Cow<'a, str>>>,
+    pub next_page_min: Option<Cow<'a, str>>,
     #[serde(rename = "NFTokens")]
     pub nftokens: Option<Vec<NFTokenMetadata<'a>>>,
+    pub previous_page_min: Option<Cow<'a, str>>,
+    pub sequence: Option<i32>,
+    pub taker_gets: Option<Amount<'a>>,
+    pub taker_pays: Option<Amount<'a>>,
     pub xchain_claim_id: Option<Cow<'a, str>>,
 }
 
 #[skip_serializing_none]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
-pub struct CreatedNodeFields<'a> {
-    pub ledger_entry_type: Cow<'a, str>,
-    pub ledger_index: Cow<'a, str>,
-    pub new_fields: Fields<'a>,
-}
-
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct CreatedNode<'a> {
-    pub created_node: CreatedNodeFields<'a>,
-}
-
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ModifiedNodeFields<'a> {
-    pub ledger_entry_type: Cow<'a, str>,
-    pub ledger_index: Cow<'a, str>,
-    pub final_fields: Option<Fields<'a>>,
-    pub previous_fields: Option<Fields<'a>>,
-    pub previous_txn_id: Option<Cow<'a, str>>,
-    pub previous_txn_lgr_seq: Option<i32>,
-}
-
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct ModifiedNode<'a> {
-    pub modified_node: ModifiedNodeFields<'a>,
-}
-
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct DeletedNodeFields<'a> {
-    pub ledger_entry_type: Cow<'a, str>,
-    pub ledger_index: Cow<'a, str>,
-    pub final_fields: Fields<'a>,
-    pub previous_fields: Option<Fields<'a>>,
-}
-
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(rename_all = "PascalCase")]
-pub struct DeletedNode<'a> {
-    pub deleted_node: DeletedNodeFields<'a>,
+pub enum AffectedNode<'a> {
+    #[serde(rename_all = "PascalCase")]
+    CreatedNode {
+        ledger_entry_type: LedgerEntryType,
+        ledger_index: LedgerIndex<'a>,
+        new_fields: Fields<'a>,
+    },
+    #[serde(rename_all = "PascalCase")]
+    ModifiedNode {
+        ledger_entry_type: LedgerEntryType,
+        ledger_index: LedgerIndex<'a>,
+        final_fields: Option<Fields<'a>>,
+        previous_fields: Option<Fields<'a>>,
+        previous_txn_id: Option<Cow<'a, str>>,
+        previous_txn_lgr_seq: Option<i32>,
+    },
+    #[serde(rename_all = "PascalCase")]
+    DeletedNode {
+        ledger_entry_type: LedgerEntryType,
+        ledger_index: LedgerIndex<'a>,
+        final_fields: Fields<'a>,
+        previous_fields: Option<Fields<'a>>,
+    },
 }
 
 #[skip_serializing_none]
 #[derive(Debug, Clone, Serialize, Deserialize)]
 #[serde(rename_all = "PascalCase")]
 pub struct TransactionMetadata<'a> {
-    pub affected_nodes: Vec<Node<'a>>,
+    pub affected_nodes: Vec<AffectedNode<'a>>,
     pub transaction_index: i32,
     pub transaction_result: Amount<'a>,
     #[serde(rename = "delivered_amount")]
     pub delivered_amount: Option<Amount<'a>>,
-    // pub delivered_amount_unavailable: Option<Cow<'a, str>>,
 }
 
-#[skip_serializing_none]
-#[derive(Debug, Clone, Serialize, Deserialize)]
-#[serde(untagged)]
-pub enum Node<'a> {
-    Created(CreatedNode<'a>),
-    Modified(ModifiedNode<'a>),
-    Deleted(DeletedNode<'a>),
+#[cfg(test)]
+mod test_serde {
+    #[test]
+    fn test_deserialize_deleted_node() {
+        let json = r#"
+            {
+                "DeletedNode": {
+                    "FinalFields": {
+                        "Account": "rHzKtpcB1KC1YuU4PBhk9m2abqrf2kZsfV",
+                        "BookDirectory": "623C4C4AD65873DA787AC85A0A1385FE6233B6DE100799474F20E441AE211B08",
+                        "BookNode": "0",
+                        "Flags": 0,
+                        "OwnerNode": "0",
+                        "PreviousTxnID": "3C5524789C20AE661CF4985EF694F11128FE267D084CB5E77DBB5FFD78E31E1C",
+                        "PreviousTxnLgrSeq": 92173580,
+                        "Sequence": 29337056,
+                        "TakerGets": "17250658754",
+                        "TakerPays": {
+                            "currency": "CNY",
+                            "issuer": "rJ1adrpGS3xsnQMb9Cw54tWJVFPuSdZHK",
+                            "value": "159709.5313591656"
+                        }
+                    },
+                    "LedgerEntryType": "Offer",
+                    "LedgerIndex": "D11F69DE8A8CACB130F2E2B9893E5C97B9EE4136759C66C1F3497C8575FF5ED0"
+                }
+            }
+        "#;
+        let deleted_node = serde_json::from_str::<super::AffectedNode>(json);
+
+        assert!(deleted_node.is_ok());
+    }
+
+    #[test]
+    fn test_deserialize_modified_node() {
+        let json = r#"
+            {
+                "ModifiedNode": {
+                    "FinalFields": {
+                        "Account": "rHzKtpcB1KC1YuU4PBhk9m2abqrf2kZsfV",
+                        "Balance": "5000542889",
+                        "Flags": 0,
+                        "OwnerCount": 5,
+                        "Sequence": 29337064
+                    },
+                    "LedgerEntryType": "AccountRoot",
+                    "LedgerIndex": "991ED60C316200D33B2EA3E56E505433394DBA7FF5E7ADE8C8850D02BEF1F53A",
+                    "PreviousFields": {
+                        "Balance": "5000542904",
+                        "Sequence": 29337063
+                    },
+                    "PreviousTxnID": "960FAFAF9CA0465B7475F888946F0D58F9CF49B18F3991D826B03A5025368DDE",
+                    "PreviousTxnLgrSeq": 92173588
+                }
+            }
+        "#;
+        let modified_node = serde_json::from_str::<super::AffectedNode>(json);
+
+        assert!(modified_node.is_ok());
+    }
+
+    #[test]
+    fn test_deserialize_created_node() {
+        let json = r#"
+            {
+                "CreatedNode": {
+                    "LedgerEntryType": "AccountRoot",
+                    "LedgerIndex": "991ED60C316200D33B2EA3E56E505433394DBA7FF5E7ADE8C8850D02BEF1F53A",
+                    "NewFields": {
+                        "Account": "rHzKtpcB1KC1YuU4PBhk9m2abqrf2kZsfV",
+                        "Balance": "5000542889",
+                        "Flags": 0,
+                        "OwnerCount": 5,
+                        "Sequence": 29337064
+                    }
+                }
+            }
+        "#;
+        let created_node = serde_json::from_str::<super::AffectedNode>(json);
+
+        assert!(created_node.is_ok());
+    }
 }
--- src/utils/get_nftoken_id.rs
+++ src/utils/get_nftoken_id.rs
@@ -1,7 +1,8 @@
 use alloc::{borrow::Cow, vec::Vec};
 
+use crate::models::ledger::objects::LedgerEntryType;
 use crate::models::transactions::metadata::TransactionMetadata;
-use crate::models::transactions::metadata::{NFTokenMetadata, Node};
+use crate::models::transactions::metadata::{AffectedNode, NFTokenMetadata};
 
 /// Flattens objects into a single list, and applies func to every object in objects.
 fn flatmap<T, R>(func: impl Fn(T) -> Vec<R>, list_of_items: Vec<T>) -> Vec<R> {
@@ -12,47 +13,54 @@ fn flatmap<T, R>(func: impl Fn(T) -> Vec<R>, list_of_items: Vec<T>) -> Vec<R> {
     modified_items
 }
 
-fn get_nftoken_ids_from_nftokens(nftokens: Vec<NFTokenMetadata<'_>>) -> Vec<Cow<'_, str>> {
+pub fn get_nftoken_ids_from_nftokens(nftokens: Vec<NFTokenMetadata<'_>>) -> Vec<Cow<'_, str>> {
     nftokens
         .into_iter()
         .map(|token| token.nftoken.nftoken_id)
         .collect()
 }
 
-fn has_nftoken_page(node: &Node) -> bool {
+fn has_nftoken_page(node: &AffectedNode) -> bool {
     match node {
-        Node::Created(node) => node.created_node.ledger_entry_type == "NFTokenPage",
-        Node::Modified(node) => {
-            node.modified_node.ledger_entry_type == "NFTokenPage"
-                && node.modified_node.previous_fields.is_some()
-                && node
-                    .modified_node
-                    .previous_fields
-                    .as_ref()
-                    .unwrap()
-                    .nftokens
-                    .is_some()
+        AffectedNode::CreatedNode {
+            ledger_entry_type, ..
+        } => ledger_entry_type == &LedgerEntryType::NFTokenPage,
+        AffectedNode::ModifiedNode {
+            ledger_entry_type,
+            previous_fields,
+            ..
+        } => {
+            ledger_entry_type == &LedgerEntryType::NFTokenPage
+                && previous_fields.is_some()
+                && previous_fields.as_ref().unwrap().nftokens.is_some()
         }
-        Node::Deleted(_) => false,
+        _ => false,
     }
 }
 
-fn get_previous_nftokens<'a>(node: &'a Node) -> Vec<NFTokenMetadata<'a>> {
-    let mut nftokens: Vec<NFTokenMetadata> = Vec::new();
-    if let Node::Modified(modified_node) = node {
-        if let Some(previous_fields) = &modified_node.modified_node.previous_fields {
-            if let Some(new_nftokens) = &previous_fields.nftokens {
-                nftokens = new_nftokens.clone();
+fn get_previous_nftokens<'a>(node: &'a AffectedNode) -> Vec<NFTokenMetadata<'a>> {
+    match node {
+        AffectedNode::ModifiedNode {
+            previous_fields, ..
+        } => {
+            if let Some(previous_fields) = previous_fields {
+                if let Some(new_nftokens) = &previous_fields.nftokens {
+                    new_nftokens.clone()
+                } else {
+                    Vec::new()
+                }
+            } else {
+                Vec::new()
             }
         }
+        _ => Vec::new(),
     }
-    nftokens
 }
 
-fn get_new_nftokens<'a>(node: &'a Node) -> Vec<NFTokenMetadata<'a>> {
+fn get_new_nftokens<'a>(node: &'a AffectedNode) -> Vec<NFTokenMetadata<'a>> {
     match node {
-        Node::Modified(modified_node) => {
-            if let Some(final_fields) = &modified_node.modified_node.final_fields {
+        AffectedNode::ModifiedNode { final_fields, .. } => {
+            if let Some(final_fields) = final_fields {
                 if let Some(new_nftokens) = &final_fields.nftokens {
                     new_nftokens.clone()
                 } else {
@@ -62,8 +70,8 @@ fn get_new_nftokens<'a>(node: &'a Node) -> Vec<NFTokenMetadata<'a>> {
                 Vec::new()
             }
         }
-        Node::Created(created_node) => {
-            if let Some(new_nftokens) = &created_node.created_node.new_fields.nftokens {
+        AffectedNode::CreatedNode { new_fields, .. } => {
+            if let Some(new_nftokens) = &new_fields.nftokens {
                 new_nftokens.clone()
             } else {
                 Vec::new()
@@ -74,7 +82,7 @@ fn get_new_nftokens<'a>(node: &'a Node) -> Vec<NFTokenMetadata<'a>> {
 }
 
 pub fn get_nftoken_id<'a: 'b, 'b>(meta: &'a TransactionMetadata<'a>) -> Option<Cow<'b, str>> {
-    let affected_nodes: Vec<&Node> = meta
+    let affected_nodes: Vec<&AffectedNode> = meta
         .affected_nodes
         .iter()
         .filter(|node| has_nftoken_page(node))
@@ -93,4 +101,49 @@ pub fn get_nftoken_id<'a: 'b, 'b>(meta: &'a TransactionMetadata<'a>) -> Option<C
     final_token_ids
         .into_iter()
         .find(|id| !previous_token_ids.contains(id))
+}
+
+#[cfg(test)]
+mod test {
+    use lazy_static::lazy_static;
+    use serde::{Deserialize, Serialize};
+
+    use crate::models::transactions::metadata::TransactionMetadata;
+
+    #[derive(Debug, Clone, Serialize, Deserialize)]
+    struct Txn {
+        pub meta: TransactionMetadata<'static>,
+    }
+
+    fn load_tests() -> &'static Option<(Txn, Txn)> {
+        pub const NFTOKEN_MINT_RESPONSE_1: &str =
+            include_str!("./test_data/transaction_jsons/nftokenmint_response1.json");
+        pub const NFTOKEN_MINT_RESPONSE_2: &str =
+            include_str!("./test_data/transaction_jsons/nftokenmint_response2.json");
+
+        lazy_static! {
+            static ref TEST_CASES: Option<(Txn, Txn,)> = Some((
+                serde_json::from_str(NFTOKEN_MINT_RESPONSE_1).expect("load_tests_1"),
+                serde_json::from_str(NFTOKEN_MINT_RESPONSE_2).expect("load_tests_2"),
+            ));
+        }
+
+        &TEST_CASES
+    }
+
+    #[test]
+    fn test_nftoken_id() {
+        let (txn1, txn2) = load_tests()
+            .as_ref()
+            .expect("test_decoding_a_valid_nftoken_id");
+        let nftoken_id1 = super::get_nftoken_id(&txn1.meta);
+        let nftoken_id2 = super::get_nftoken_id(&txn2.meta);
+        let expected_nftoken_id1 =
+            Some("00081388DC1AB4937C899037B2FDFC3CB20F6F64E73120BB5F8AA66A00000228".into());
+        let expected_nftoken_id2 =
+            Some("0008125CBE4B401B2F62ED35CC67362165AA813CCA06316FFA766254000003EE".into());
+
+        assert_eq!(nftoken_id1, expected_nftoken_id1);
+        assert_eq!(nftoken_id2, expected_nftoken_id2);
+    }
 }
